53dc9c67d129159034ea65f89e7960e9fcfd1e22
Arnaud Levy <contact@arnaudlevy.com>
2021-01-12 19:26:32 +0100

    keyup

--- summernote-current-style.js
+++ summernote-current-style.js
@@ -1,4 +1,4 @@
-/*! summernote-current-style v1.0.5 */
+/*! summernote-current-style v1.0.6 */
 (function (factory) {
   if (typeof define === 'function' && define.amd) {
     define(['jquery'], factory);
@@ -13,17 +13,19 @@
     'current-style': function (context) {
       var self = this,
           $editor = context.layoutInfo.editor;
+      this.highlight = function (we, e) {
+        var node = e.target.nodeName.toLowerCase();
+        e.stopPropagation();
+        // Remove active class from old list
+        $editor.find('.dropdown-style .dropdown-item').removeClass('active');
+        // Remove active class from new list
+        $editor.find('.dropdown-style li').removeClass('active');
+        // Add active class to current style
+        $editor.find('.dropdown-style [aria-label="' + node + '"]').addClass('active');
+      };
       this.events = {
-        'summernote.mousedown': function (we, e) {
-          var node = e.target.nodeName.toLowerCase();
-          e.stopPropagation();
-          // Remove active class from old list
-          $editor.find('.dropdown-style .dropdown-item').removeClass('active');
-          // Remove active class from new list
-          $editor.find('.dropdown-style li').removeClass('active');
-          // Add active class to current style
-          $editor.find('.dropdown-style [aria-label="' + node + '"]').addClass('active');
-        }
+        'summernote.mousedown': this.highlight,
+        'summernote.keyup': this.highlight
       }
     }
   });
